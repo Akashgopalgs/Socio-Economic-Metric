cf67c43049fab1fce3e1490ece24bba39b05d35a
Akash Gopal GS <154494528+Akashgopalgs@users.noreply.github.com>
2024-09-04 18:42:46 +0530

    Delete tesk.py

--- tesk.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-df= pd.read_csv('dataset/iris.csv')
-
-iris_dataset = st.multiselect(label='Features of Iris-Flower ',
-                             options=[df.columns[0],df.columns[1],df.columns[2],df.columns[3],df.columns[4]],
-                              default=df.columns[4])
